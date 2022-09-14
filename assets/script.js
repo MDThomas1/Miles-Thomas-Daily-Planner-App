62da0b22fe44d9c8cc4b722b6d23fc24c9f8d7f0
Miles Thomas <milesman2013@gmail.com>
2022-09-14 16:00:20 +1000

    start-function

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,15 @@
 var timer = moment()
+var saveBtn = document.querySelectorAll('.saveBtn')
+var plans = document.querySelectorAll('textarea')
+var hourTag = document.querySelectorAll('.hour')
+
+saveBtn.on('click', function saveEvents() {
+    var saved = localStorage.setItem(plans);
+    $('textarea').text(localStorage.getItem('activity', saved))  
+});
+
 $('#currentDay').text(timer.format("[Today is ] dddd, MMM Do, YYYY"))
+
+function ppf() {
+
+}
